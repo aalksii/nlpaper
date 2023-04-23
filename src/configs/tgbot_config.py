7405ddbc6c727c43d187f6891a434a9e16939b46
Aleksei Artemiev <34196081+aalksii@users.noreply.github.com>
2023-04-23 22:21:44 +0300

    Change config

--- src/configs/tgbot_config.py
+++ src/configs/tgbot_config.py
@@ -4,4 +4,4 @@ downloaded_path = Path('downloaded')
 input_file_name = Path('input_paper.pdf')
 output_file_name = Path('output_paper.pdf')
 limit_sentences = 5
-use_webhooks = False
+use_webhooks = True
