4cde210413b238722fa526284b42a47eb68bcfdd
Aleksei Artemiev <34196081+aalksii@users.noreply.github.com>
2023-04-23 18:48:10 +0300

    Add file processing to bot

--- aiogrambot.py
+++ aiogrambot.py
@@ -1,10 +1,13 @@
-import io
 import logging
 import os
+from pathlib import Path
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
+
+from configs import *
+from utils import highlight_ranked
 
 TOKEN = os.getenv('TG_BOT_TOKEN')
 bot = Bot(token=TOKEN)
@@ -41,19 +44,33 @@ async def send_welcome(message: types.Message):
     await message.reply(welcome_text)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-
 @dp.message_handler(content_types=['document'])
 async def document_handler(message: types.Message):
-    file_in_io = io.BytesIO()
-    if message.content_type == 'document':
-        await message.document.download(destination_file=file_in_io)
-    print(file_in_io)
+    if document := message.document:
+        file_name = Path(document.file_name)
+        if file_name.suffix == '.pdf':
+            await message.reply('⏳ Please wait, your file is being processed')
 
-    await message.answer('⏳ Please wait, your file is being processed')
+            if not os.path.isdir(downloaded_path):
+                os.makedirs(downloaded_path)
+            await document.download(
+                destination_file=downloaded_path / input_file_name
+            )
+
+            sentences = highlight_ranked(
+                str(downloaded_path / input_file_name),
+                str(downloaded_path / output_file_name),
+                limit_sentences=limit_sentences)
+            sentences_text = "\n 👉 ".join([''] + sentences)
+            text = f'🔥 Top {limit_sentences} most important sentences in ' \
+                   f'the text:\n{sentences_text}'
+            await message.reply(text)
+
+            doc = open(downloaded_path / output_file_name, 'rb')
+            await message.reply_document(doc)
+
+        else:
+            await message.reply('☹️ We can process PDF files only.')
 
 
 if __name__ == '__main__':
@@ -67,3 +84,4 @@ if __name__ == '__main__':
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
+    # executor.start_polling(dp, skip_updates=True)
