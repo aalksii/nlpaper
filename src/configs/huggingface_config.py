37d47a01d80485d61bcfcd7e4153e2589312b6cf
Aleksei Artemiev <34196081+aalksii@users.noreply.github.com>
2023-05-18 15:43:42 +0300

    Add dataset pull

---
+++ src/configs/huggingface_config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+dataset_path = Path('../data/ml-arxiv-papers/')
+dataset_path_huggingface = 'aalksii/ml-arxiv-papers'
+select_ratio = 0.001
+cache_dir = Path('../.cache/huggingface/')
--- src/pull_data.py
+++ src/pull_data.py
@@ -1,2 +1,32 @@
+from datasets import load_dataset
+from configs.huggingface_config import (
+    dataset_path, dataset_path_huggingface, select_ratio, cache_dir
+)
+
+
+def filter_none(example):
+    return example['abstract'] is not None and len(example['abstract'])
+
+
+def prepare_dataset():
+    # Download dataset
+    dataset = load_dataset(dataset_path_huggingface,
+                           cache_dir=cache_dir)
+    train = dataset['train']
+    test = dataset['test']
+
+    # Select a part of the dataset
+    train = train.select(range(int(select_ratio * len(train))))
+    test = test.select(range(int(select_ratio * len(test))))
+
+    # Filter None values and empty strings
+    train = train.filter(filter_none)
+    test = test.filter(filter_none)
+
+    # Save datasets
+    train.save_to_disk(dataset_path / 'train')
+    test.save_to_disk(dataset_path / 'test')
+
+
 if __name__ == '__main__':
-    print('Downloading data...')
+    prepare_dataset()
