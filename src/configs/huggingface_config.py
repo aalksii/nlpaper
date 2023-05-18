e2265a89f36113bc04789ae558cc87b2bc172396
Aleksei Artemiev <34196081+aalksii@users.noreply.github.com>
2023-05-18 20:36:12 +0300

    Update huggingface_config.py

--- src/configs/huggingface_config.py
+++ src/configs/huggingface_config.py
@@ -2,13 +2,13 @@ from pathlib import Path
 
 dataset_path = Path('data/ml-arxiv-papers/')
 dataset_path_hf = 'aalksii/ml-arxiv-papers'
-select_ratio = 0.001
+select_ratio = 0.1
 test_size = 1 / 9
 cache_dir = Path('cache/huggingface/')
 model_checkpoint_hf = 'aalksii/distilbert-base-uncased-ml-arxiv-papers'
-chunk_size = 16
+chunk_size = 128
 wwm_probability = 0.15
-batch_size = 16
+batch_size = 128
 trained_model_name = 'distilbert-base-uncased-ml-arxiv-papers-trained'
 trained_model_path = Path('models/') / trained_model_name
 push_to_hub = False
