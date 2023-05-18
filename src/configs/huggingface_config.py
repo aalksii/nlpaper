45dc9634c247f939a4d81ea4eacdf5aab2f10eef
Aleksei Artemiev <34196081+aalksii@users.noreply.github.com>
2023-05-18 19:06:38 +0300

    Update huggingface_config.py

--- src/configs/huggingface_config.py
+++ src/configs/huggingface_config.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
-dataset_path = Path('../data/ml-arxiv-papers/')
+dataset_path = Path('data/ml-arxiv-papers/')
 dataset_path_hf = 'aalksii/ml-arxiv-papers'
 select_ratio = 0.001
-cache_dir = Path('../cache/huggingface/')
+cache_dir = Path('cache/huggingface/')
 model_checkpoint_hf = 'aalksii/distilbert-base-uncased-ml-arxiv-papers'
 chunk_size = 16
 wwm_probability = 0.15
 batch_size = 16
 trained_model_name = 'distilbert-base-uncased-ml-arxiv-papers-trained'
-trained_model_path = Path('../models/') / trained_model_name
+trained_model_path = Path('models/') / trained_model_name
 push_to_hub = False
 fp16 = False  # Use it for GPU
 wwm_collator = True
