4704ecacb512aadcc832f771451af16af86330e4
Aleksei Artemiev <34196081+aalksii@users.noreply.github.com>
2023-05-21 21:31:29 +0300

    Fix ports and add nohup

--- src/request.py
+++ src/request.py
@@ -1,12 +1,14 @@
 import requests
 
-x = requests.post('http://127.0.0.1:5000/api/fill_mask', json={
+from configs.server_config import rest_api_port
+
+x = requests.post(f'http://127.0.0.1:{rest_api_port}/api/fill_mask', json={
     'x': 'New study links disturbed [MASK] metabolism in depressed '
          'individuals to disruption of the gut microbiome.'
 })
 print('fill mask:', x.text)
 
-x = requests.post('http://127.0.0.1:5000/api/summarize', json={
+x = requests.post(f'http://127.0.0.1:{rest_api_port}/api/summarize', json={
     'x': 'Depression is a widespread mental health condition '
          'that significantly affects population health. Major '
          'depression is known to cause a range of debilitating '
